e7f4fca6fc834fb8a7699de6e5aea988bf32484f
Mahidhar <g.m.mahidharreddy@gmail.com>
2017-07-30 20:53:27 +0200

    commiting new files:30-July-2017

--- src/main/java/com/k15t/pat/registration/RegistrationController.java
+++ src/main/java/com/k15t/pat/registration/RegistrationController.java
@@ -1,29 +1,89 @@
 package com.k15t.pat.registration;
 
-import org.apache.velocity.Template;
-import org.apache.velocity.VelocityContext;
-import org.apache.velocity.app.VelocityEngine;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.StringWriter;
+import com.k15t.pat.domain.Registration;
+import com.k15t.pat.exception.CustomGenericException;
+import com.k15t.pat.service.RegistrationService;
 
+/**
+ * This controller is the Rest Controller API
+ * 
+ * @author Mahidhar
+ *
+ */
 
 @RestController
+@RequestMapping("/registrations")
 public class RegistrationController {
 
-    @Autowired private VelocityEngine velocityEngine;
+	@Autowired
+	private RegistrationService registrationService;
 
+	/**
+	 * This method returns the list of all stored registrations
+	 * 
+	 * @return
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public Iterable<Registration> list() {
+		return registrationService.list();
+	}
 
-    @RequestMapping("/registration.html")
-    public String registration() {
+	/**
+	 * This method inserts a new registration entry in the database
+	 * 
+	 * @param registration
+	 * @return instance of Registration
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.POST)
+	public Registration create(@RequestBody Registration registration) {
+		return registrationService.create(registration);
+	}
 
-        Template template = velocityEngine.getTemplate("templates/registration.vm");
-        VelocityContext context = new VelocityContext();
-        StringWriter writer = new StringWriter();
-        template.merge(context, writer);
+	/**
+	 * This method takes the id and returns the matching registration fields
+	 * 
+	 * @param id
+	 * @return instance of Registration
+	 */
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public Registration read(@PathVariable(value = "id") long id) {
+		Registration registration = registrationService.read(id);
+		if (registration == null) {
+			throw new CustomGenericException(null, "registration with id: " + id + " not found.");
+		}
+		return registration;
+	}
 
-        return writer.toString();
-    }
+	/**
+	 * This method takes the id and updates the values with the matched instance
+	 * in the database
+	 * 
+	 * @param id
+	 * @param registration
+	 * @return instance of Registration
+	 */
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public Registration update(@PathVariable(value = "id") long id, @RequestBody Registration registration) {
+		return registrationService.update(id, registration);
+	}
+
+	/**
+	 * This method takes the id and deletes the matching instance from the
+	 * database
+	 * 
+	 * @param id
+	 */
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public void delete(@PathVariable(value = "id") int id) {
+		registrationService.delete(id);
+	}
+
 }
